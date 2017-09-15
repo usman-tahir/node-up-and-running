d8453657162623263b13d12fd1199d09036868c5
usman-tahir <tahir.usman.ali94@outlook.com>
2017-09-15 19:20:52 -0400

    feat: added a basic Node server example

---
+++ first-server/server.js
@@ -0,0 +1,15 @@
+
+(function() {
+
+	'use strict';
+
+	var http = require('http');
+
+	http.createServer(function(req, res) {
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+		res.end('hello world\n');
+	}).listen(8080, '127.0.0.1');
+
+	console.log('Server running at http://localhost:8080');
+
+}());
