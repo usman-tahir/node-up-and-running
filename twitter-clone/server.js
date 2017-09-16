61dce2e2f88f96e2b7a5a34247c0ce7129a1f8da
usman-tahir <tahir.usman.ali94@outlook.com>
2017-09-16 12:21:55 -0400

    feat: defined the /tweets (get) and /send (post) routes for the Node Twitter app

--- twitter-clone/server.js
+++ twitter-clone/server.js
@@ -4,12 +4,29 @@
 	'use strict';
 
 	var express = require('express'),
-		app = express();
+		app = express(),
+		bodyParser = require('body-parser'),
+		tweets = [];
+
+	app.use(bodyParser.json());
+	app.listen(8080);
 
 	app.get('/', function(req, res) {
 		res.send('Welcome to Node Twitter');
 	});
 
-	app.listen(8080);
+	app.post('/send', function(req, res) {
+		if (req.body && req.body.tweet) {
+			tweets.push(req.body.tweet);
+			res.send({status: 'ok', message: 'Tweet received!'});
+		} else {
+			// When no Tweet was sent
+			res.send({status: 'nok', message: 'No Tweet received.'});
+		}
+	});
+
+	app.get('/tweets', function(req, res) {
+		res.send(tweets);
+	});
 
 }());
