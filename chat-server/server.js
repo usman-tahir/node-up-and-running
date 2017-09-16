86d87506c8365369417b6f2954802afacb1ee118
usman-tahir <tahir.usman.ali94@outlook.com>
2017-09-16 09:36:41 -0400

    feat: refactored write capabilities of sockets

--- chat-server/server.js
+++ chat-server/server.js
@@ -9,22 +9,37 @@
 
 	chatServer.on('connection', function(client) {
 		client.name = client.remoteAddress + ':' + client.remotePort;
-		client.write('Hi, ' + client.name + '!\n');
+		client.write(client.name + ' has joined.\n');
 
 		clientList.push(client);
 
 		client.on('data', function(data) {
 			broadcast(data, client);
 		});
+
+		client.on('end', function() {
+			clientList.splice(clientList.indexOf(client), 1);
+		});
 	});
 
 	function broadcast(message, client) {
-		var i;
+		var cleanup = [],
+			i;
 
 		for (i = 0; i < clientList.length; i += 1) {
 			if (client !== clientList[i]) {
-				clientList[i].write(client.name + ' says ' + message);
+				if (clientList[i].writable) {
+					clientList[i].write(client.name + ': ' + message);
+				} else {
+					cleanup.push(clientList[i]);
+					clientList[i].destroy();
+				}
 			}
+		}
+
+		// Remove the dead Nodes out of the write loop
+		for(i = 0; i < cleanup.length; i += 1) {
+			clientList.splice(clientList.indexOf(cleanup[i]), 1);
 		}
 	}
 
