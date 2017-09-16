7511512fb4b10bb52bd688c17ca257be3068029c
usman-tahir <tahir.usman.ali94@outlook.com>
2017-09-15 20:58:12 -0400

    feat: added functionality for pushing messages to clients

--- chat-server/server.js
+++ chat-server/server.js
@@ -4,13 +4,21 @@
 	'use strict';
 
 	var net = require('net'),
-		chatServer = net.createServer();
+		chatServer = net.createServer(),
+		clientList = [];
 
 	chatServer.on('connection', function(client) {
 		client.write('Hi!\n');
 
+		clientList.push(client);
+
 		client.on('data', function(data) {
-			console.log(data);
+			var i;
+
+			for(i = 0; i < clientList.length; i += 1) {
+				// write the data to all clients
+				clientList[i].write(data);
+			}
 		});
 	});
 
